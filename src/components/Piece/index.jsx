658f3231b5682ab66035d4017d62e020390e94d8
get.Friday <gabriel.getdev@gmail.com>
2023-03-12 16:03:42 -0300

    Fix: Prepare drag event import useRef

--- src/components/Piece/index.jsx
+++ src/components/Piece/index.jsx
@@ -1,13 +1,15 @@
+import { useRef } from 'react';
 import './styles.css';
 
 export function Piece(props) {
+  const dragPiece = useRef();
   const classes = `piece ${props.piece} square-${props.square}`;
 
-  const handleDrag = () => {
-    console.log('call');
+  const handleDragStart = (event) => {
+    event.preventDefault();
   }
 
   return(
-    <div className={classes} onDrag={handleDrag} />
+    <div className={classes} onDragStart={handleDragStart}/>
   )
 }
